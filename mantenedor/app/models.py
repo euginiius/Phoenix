08a666cfd23fbf93ce2311291477663010a4df3c
unknown <eugeniioa@gmail.com>
2019-11-17 00:41:18 -0300

    respaldo en caso de que deje la caga

--- mantenedor/app/models.py
+++ mantenedor/app/models.py
@@ -13,3 +13,14 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class DatosCliente(models.Model):
+    region= models.TextField()
+    comuna= models.TextField()
+    nombre_cliente= models.CharField(max_length=250)
+    email= models.CharField(max_length=100)
+    telefono= models.CharField(max_length=13)
+    tipo_vivienda= models.TextField()
+
+    def __str__(self):
+        return self.nombre_cliente
